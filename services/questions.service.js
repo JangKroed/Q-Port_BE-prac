bb381de9d8e746bd8ada60fdde662e64a96dc6be
JangKroed <didlsdydgh@gmail.com>
2022-10-22 21:02:44 +0900

    question service

---
+++ services/questions.service.js
@@ -0,0 +1,96 @@
+const QuestionsRepository = require('../repositories/questions.repository');
+
+class QuestionsService {
+  questionsRepository = new QuestionsRepository();
+
+  createQna = async (req, res, next) => {
+    const { userId } = res.locals.user;
+    const { title, content, imgUrl } = req.body;
+
+    const qna = {
+      userId,
+      title,
+      content,
+      imgUrl,
+    };
+
+    await this.questionsRepository.createQna(qna);
+
+    res.status(200);
+  };
+
+  getQna = async (req, res, next) => {
+    return await this.questionsRepository.getQna();
+  };
+
+  findByQna = async (req, res, next) => {
+    const { questionId } = req.params;
+    const detail = await this.questionsRepository.findByQna(questionId);
+    if (!questionId || !detail) throw new Error('잘못된 요청 입니다.');
+
+    return detail;
+  };
+
+  updateQna = async (req, res, next) => {
+    const { questionId } = req.params;
+    const { title, content, imgUrl } = req.body;
+    const { userId } = res.locals.user;
+    const findByWriter = await this.questionsRepository.findByQna(questionId);
+
+    if (findByWriter.userId !== userId)
+      throw new Error('본인만 수정할 수 있습니다.');
+    if (!findByWriter) throw new Error('잘못된 요청입니다.');
+
+    await this.questionsRepository.updateQna(
+      questionId,
+      title,
+      content,
+      imgUrl
+    );
+  };
+
+  deleteQna = async (req, res, next) => {
+    const { userId } = res.locals.user;
+    const { questionId } = req.params;
+    const findByWriter = await this.questionsRepository.findByQna(questionId);
+
+    if (findByWriter.userId !== userId)
+      throw new Error('본인만 수정할 수 있습니다.');
+    if (!findByWriter) throw new Error('잘못된 요청입니다.');
+
+    await this.questionsRepository.deleteQna(questionId);
+  };
+
+  selectQna = async (req, res, next) => {
+    const { questionId, answerId } = req.params;
+    const { userId } = res.locals.user;
+
+    const findByWriter = await this.questionsRepository.findByQna(questionId);
+
+    if (!findByWriter) throw new Error('잘못된 요청입니다.');
+    if (findByWriter.userId !== userId)
+      throw new Error('본인만 채택할 수 있습니다.');
+
+    await this.questionsRepository.selectQna(questionId, answerId);
+  };
+
+  updateImage = async (userId, questionId, imageFileName) => {
+    if (!imageFileName) throw new Error('게시물 이미지가 빈 값');
+    const findByWriter = await this.questionsRepository.findByQna(questionId);
+
+    if (findByWriter.userId !== userId)
+      throw new Error('본인만 수정할 수 있습니다.');
+    if (!findByWriter) throw new Error('잘못된 요청입니다.');
+
+    const updateImageData = await this.questionsRepository.updateImage(
+      questionId,
+      imageFileName
+    );
+    // require 안했는데 이게 됨 ?
+    updateImageData.imgUrl =
+      process.env.S3_STORAGE_URL + updateImageData.imgUrl;
+    return updateImageData;
+  };
+}
+
+module.exports = QuestionsService;
