6f438f3271ce3c872ad6301a67a968677bf933e5
JangKroed <didlsdydgh@gmail.com>
2022-10-22 21:03:22 +0900

    question repository

---
+++ repositories/questions.repository.js
@@ -0,0 +1,73 @@
+const { Question } = require('../models');
+
+class QuestionsRepository {
+  constructor() {
+    this.Question = Question;
+  }
+
+  createQna = async (qna) => {
+    await this.Question.create({
+      ...qna,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    });
+  };
+
+  getQna = async () => {
+    return await this.Question.findAll({
+      attributes: {
+        exclude: ['content'],
+      },
+      order: [['createdAt', 'desc']],
+    });
+  };
+
+  findByQna = async (questionId) => {
+    return await this.Question.findOne({
+      where: {
+        questionId,
+      },
+    });
+  };
+
+  updateQna = async (questionId, title, content, imgUrl) => {
+    await this.Question.update(
+      { title, content, imgUrl, updatedAt: Date.now() },
+      { where: { questionId } }
+    );
+  };
+
+  deleteQna = async (questionId) => {
+    await this.Question.destroy({
+      where: {
+        questionId,
+      },
+    });
+  };
+
+  selectQna = async (questionId, answerId) => {
+    await this.Question.update(
+      { selectedAnswer: answerId },
+      { where: { questionId } }
+    );
+  };
+
+  updateImage = async (questionId, imgUrl) => {
+    try {
+      await this.Question.update(
+        {
+          imgUrl,
+        },
+        {
+          where: { questionId },
+        }
+      );
+
+      return await this.findByQna(questionId);
+    } catch (error) {
+      throw new Error(`UnhandleMysqlSequelizeError: ${error}`);
+    }
+  };
+}
+
+module.exports = QuestionsRepository;
